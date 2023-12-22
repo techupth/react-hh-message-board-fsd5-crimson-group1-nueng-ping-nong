21a9836ca0958356e1a9cf4c8cb0490285013629
bodin <bodinsonsawan@gmail.com>
2023-12-22 15:05:14 +0700

    bodin's asssingment

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,24 +1,67 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [message, setMessage] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
+
+  const handleMessageChange = (event) => {
+    setInputMessage(event.target.value);
+  };
+
+  const addMessage = (event) => {
+    event.preventDefault();
+    const newMessage = [...message];
+    newMessage.push(inputMessage);
+    setMessage(newMessage);
+  };
+
+  const deleteMessage = (messageIndex) => {
+    const newMessage = [...message];
+    newMessage.splice(messageIndex, 1);
+    setMessage(newMessage);
+  };
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
-        <label>
-          <input
-            id="message-text"
-            name="message-text"
-            type="text"
-            placeholder="Enter message here"
-          />
-        </label>
-        <button className="submit-message-button">Submit</button>
-      </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
+      <h1 className="app-title">Message board</h1>
+      <form>
+        <div className="message-input-container">
+          <label>
+            <input
+              id="message-text"
+              name="message-text"
+              type="text"
+              placeholder="Enter message here"
+              onChange={handleMessageChange}
+              value={inputMessage}
+            />
+          </label>
+          <button className="submit-message-button" onClick={addMessage}>
+            Submit
+          </button>
         </div>
-      </div>
+        <div className="board">
+          <div className="message">
+            <ul className="message-list">
+              {message.map((item, index) => {
+                return (
+                  <li>
+                    {item}{" "}
+                    <button
+                      className="delete-button"
+                      onClick={() => {
+                        deleteMessage(index);
+                      }}
+                    >
+                      x
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
